aeb180ed4cbaa0c707ac746c6d4f5ac87885e7d9
Gilles-ve <gillesveup07@gmail.com>
2026-09-03 00:10:27 +0200

    Persist autoclicker settings

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from PySide6.QtGui import (
 from PySide6.QtCore import (
     QTimer, 
     Qt,
+    QSettings,
 )
 
 from PySide6.QtWidgets import (
@@ -55,11 +56,17 @@ class MainWindow(QMainWindow):
         # Settings
         # -------------------------
 
+        self.settings = QSettings(
+        "Gander Studio",
+        "Gander AutoClicker"
+        )
+
         settings_layout = QFormLayout()
 
         self.cps_input = QSpinBox()
         self.cps_input.setRange(1, 500)
-        self.cps_input.setValue(10)
+        saved_cps = self.settings.value("cps", 10, type=int)
+        self.cps_input.setValue(saved_cps)
 
         self.mouse_button_input = QComboBox()
         self.mouse_button_input.addItems([
@@ -67,8 +74,18 @@ class MainWindow(QMainWindow):
             "Right",
             "Middle",
         ])
+        saved_button = self.settings.value(
+        "mouse_button",
+        "Left",
+        type=str
+        )
+        self.mouse_button_input.setCurrentText(saved_button)
 
-        hotkey = "F6"
+        hotkey = self.settings.value(
+        "hotkey",
+        "F6",
+        type=str
+        )
         self.hotkey_input = QKeySequenceEdit()
         self.hotkey_input.setKeySequence(QKeySequence(hotkey))
         settings_layout.addRow("Clicks per second:", self.cps_input)
@@ -90,9 +107,16 @@ class MainWindow(QMainWindow):
             QKeySequence(hotkey),
             self
         )
+
+        # -------------------------
+        # Connection to functions
+        # -------------------------
+
         self.hotkey_input.keySequenceChanged.connect(self.update_hotkey)
         self.toggle_shortcut.activated.connect(self.toggle_autoclicker)
         self.cps_input.valueChanged.connect(self.update_timer)
+        self.cps_input.valueChanged.connect(self.save_cps)
+        self.mouse_button_input.currentTextChanged.connect(self.save_mouse_button)
 
 
 
@@ -118,6 +142,10 @@ class MainWindow(QMainWindow):
 
 
 
+        # -------------------------
+        # functions
+        # -------------------------
+
     def toggle_autoclicker(self):
         self.is_running = not self.is_running
 
@@ -130,8 +158,11 @@ class MainWindow(QMainWindow):
             self.status_label.setText("Status: Stopped")
             self.click_timer.stop()
 
+
     def update_hotkey(self, new_hotkey):
         self.toggle_shortcut.setKey(new_hotkey)
+        self.settings.setValue("hotkey", new_hotkey.toString())
+
 
     def update_timer(self, cps):
         new_interval = max(
@@ -140,9 +171,18 @@ class MainWindow(QMainWindow):
         )
         self.click_timer.setInterval(new_interval)
 
+
+    def save_cps(self, cps):
+        self.settings.setValue("cps", cps)
+
+    def save_mouse_button(self, button):
+        self.settings.setValue("mouse_button", button)
+
+
     def timer_tick(self):
         button = self.mouse_button_input.currentText()
         self.mouse_backend.click(button)
+
 
 
 app = QApplication(sys.argv)
